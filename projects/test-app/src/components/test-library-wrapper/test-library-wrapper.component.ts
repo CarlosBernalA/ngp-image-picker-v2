d7f6be0e5d0acd51d6904533dbf24d3e6ee13508
CarlosBernalA <57161416+CarlosBernalA@users.noreply.github.com>
2023-06-17 19:42:41 -0500

    Initial commit

---
+++ projects/test-app/src/components/test-library-wrapper/test-library-wrapper.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ImagePickerConf } from 'projects/ngp-image-picker/src/public-api';
+@Component({
+  selector: 'app-test-library-wrapper',
+  templateUrl: './test-library-wrapper.component.html',
+  styleUrls: ['./test-library-wrapper.component.scss'],
+})
+export class TestLibraryWrapperComponent implements OnInit {
+  config2: ImagePickerConf = {
+    borderRadius: '8px',
+    language: 'es',
+    width: '330px',
+    objectFit: 'contain',
+    aspectRatio: 4 / 3,
+    compressInitial: null,
+  };
+  initialImage: string = "https://josealejandro2928.github.io/portfolio/archives/pokedex/1.jpeg";
+  // initialImage: string = "https://local-spaces.fra1.digitaloceanspaces.com/test.jpg";
+  imageSrc: any = '';
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  onImageChanged(dataUri: string) {
+    this.imageSrc = dataUri;
+  }
+}
